dae1707e60023e1e2890478ac2630c4bbef4b0c2
blabla-my <1042244638@qq.com>
2023-03-20 03:24:51 -0400

    Fixing bugs

--- carla_ros_bridge/src/carla_ros_bridge/bridge.py
+++ carla_ros_bridge/src/carla_ros_bridge/bridge.py
@@ -607,7 +607,9 @@ def main():
                 with open(parameters["town"]) as od_file:
                     data = od_file.read()
                 carla_world = carla_client.generate_opendrive_world(str(data),
-                                                                    parameters=(2.0, 50.0, 0.0, 0.6, true, true))
+                                                                    carla.OpendriveGenerationParameters(
+                                                                        wall_height=0,
+                                                                    ))
             else:
                 if carla_world.get_map().name != parameters["town"]:
                     rospy.loginfo("Loading town '{}' (previous: '{}').".format(
